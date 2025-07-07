76852364a4fd08c225cf0c695ea3e91511031856
nehapatil7 <nehapatil0507@gmail.com>
2025-07-07 23:34:52 +0530

    init commit

---
+++ src/Arrays/SubArray.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class SubArray {
+    public static int findLargestElement(int array[]){
+        int largestElement = array[0];
+        for(int i=0; i<array.length; i++){
+            if (array[i]>largestElement) {
+                largestElement = array[i];
+            }
+        }
+        return largestElement;
+    }
+    public static void main(String[] args) {
+        int arr[]={20, 45, 23, 11, 75};
+
+        int largestValue =findLargestElement(arr);
+        System.out.println("Largest Element :" + largestValue);
+    }
+}
