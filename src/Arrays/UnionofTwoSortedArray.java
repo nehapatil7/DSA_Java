6e1305fd6d554095c9e962b5d780b51e0a33fdf1
nehapatil7 <nehapatil0507@gmail.com>
2025-07-09 23:41:39 +0530

    init commit

---
+++ src/Arrays/UnionofTwoSortedArray.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class UnionofTwoSortedArray {
+    public static int sumArray(int[] arr){
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum=sum+arr[i];
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.println("Enter the size of the array:");
+        int size=scanner.nextInt();
+
+        int arr[]=new int[size];
+        System.out.println("Enter the array values one by one");
+        for (int i=0; i<size;i++){
+            arr[i]=scanner.nextInt();
+        }
+
+        int sum = sumArray(arr);
+        System.out.print("Sum of Array: "+ sum);
+
+    }
+}
